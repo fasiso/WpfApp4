03331849bbe00ae10bb824cd97bb91322cae2264
isp44 <isp44@LAB27-05>
2026-02-19 14:21:33 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.ModelsDB;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadDBInListView();
+        }
+
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+        void LoadDBInListView()
+        {
+            using (User50Context _db = new User50Context())
+            {
+                int selectedIndex = ListView1.SelectedIndex;
+                ListView1.ItemsSource = _db.Agents.ToList();
+                ListView2.ItemsSource = _db.Clients.ToList();
+                if (selectedIndex != -1)
+                {
+                    if (selectedIndex == ListView1.Items.Count) selectedIndex--;
+                    ListView1.SelectedIndex = selectedIndex;
+                    ListView1.ScrollIntoView(ListView1.SelectedItem);
+                }
+                ListView1.Focus();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Add f = new Add();
+            f.ShowDialog();
+        }
+    }
+}
+        
+        
+    
+
+
---
+++ WpfApp1/ModelsDB/User50Context.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WpfApp1.ModelsDB;
+
+public partial class User50Context : DbContext
+{
+    public User50Context()
+    {
+    }
+
+    public User50Context(DbContextOptions<User50Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Agent> Agents { get; set; }
+
+    public virtual DbSet<Client> Clients { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=192.168.24.15; Database=user50; User=user50; Password=26643; Encrypt=false");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Agent>(entity =>
+        {
+            entity.ToTable("agents");
+        });
+
+        modelBuilder.Entity<Client>(entity =>
+        {
+            entity.ToTable("clients");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
